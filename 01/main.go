1caf3505b59e0a66905a201ddbb365767f23093d
Shayan Effati <shayan@effati.com>
2025-12-02 01:08:12 +0100

    day 1 - part 2

--- 01/main.go
+++ 01/main.go
@@ -16,7 +16,13 @@ func main() {
 	}
 
 	splittedInput := strings.Split(string(input), "\n")
+	for _, line := range splittedInput {
+		if line == "" || len(line) < 2 {
+			panic("invalid line: " + line)
+		}
+	}
 	fmt.Println(part1(splittedInput))
+	fmt.Println(part2(splittedInput))
 }
 
 func part1(input []string) int {
@@ -24,9 +30,6 @@ func part1(input []string) int {
 	numberOfZeroes := 0
 
 	for _, line := range input {
-		if line == "" || len(line) < 2 {
-			panic("invalid line: " + line)
-		}
 		steps, err := strconv.Atoi(line[1:])
 		if err != nil {
 			panic(err)
@@ -44,4 +47,32 @@ func part1(input []string) int {
 	}
 
 	return numberOfZeroes
+}
+
+// I started out with negative numbers and mod and floors etc, but I gave up and just brute forced it
+func part2(input []string) int {
+	dial := dialStart
+	zeroes := 0
+	for _, line := range input {
+		steps, err := strconv.Atoi(line[1:])
+		if err != nil {
+			panic(err)
+		}
+
+		for range steps {
+			if line[0] == 'R' {
+				dial++
+			} else {
+				dial--
+			}
+
+			dial = dial % 100
+
+			if dial == 0 {
+				zeroes++
+			}
+		}
+	}
+
+	return zeroes
 }
